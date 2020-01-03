787f81aad14825b4a35d7248d398696fa66597c1
Jongkyu_homePC <wonjongkyu@naver.com>
2020-01-03 23:50:49 +0900

    project_a test

---
+++ src/ch1/project_a/index.js
@@ -0,0 +1,11 @@
+const colore = require('colors')
+
+const s1 = "안녕하세요"
+const s2 = "저는"
+const s3 = "원oo 입니다"
+const s4 = "잘 부탁드려요"
+
+console.log(s1.underline.red)
+console.log(s2.inverse.blue)
+console.log(s3.rainbow)
+console.log(s4.inverse.red)
