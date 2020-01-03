b9199df99eff8f31c774be3f36f236b87bd9619e
Jongkyu_homePC <wonjongkyu@naver.com>
2020-01-03 17:23:53 +0900

    커밋 테스트

---
+++ src/ch1/request-downloadfile.js
@@ -0,0 +1,7 @@
+
+// 모듈을 읽어 들입니다.
+const fs = require('fs')
+const request = require('request')
+
+// request 모듈을 사용해 파일을 다운로드합니다.
+request('http://uat.pw/shodou/img/28/214.png').pipe(fs.createWriteStream('test.png'))
